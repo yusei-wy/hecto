efbccce20fd7a4f826e4d51a6bf92b0924798d84
Yusei Kasahara <ykasahara@curucuru.jp>
2021-06-02 13:51:09 +0900

    add: 画面中央に welcome message を表示

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,8 @@
 use crate::terminal::Terminal;
 use termion::event::Key;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
@@ -43,11 +45,27 @@ impl Editor {
     }
 
     fn draw_rows(&self) {
+        let height = self.terminal.size().height;
         // 最後の行にチルダが表示されるように -1
-        for _ in 0..self.terminal.size().height - 1 {
+        for row in 0..height - 1 {
             Terminal::clear_current_line();
-            println!("~\r");
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r");
+            }
         }
+    }
+
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Hecto editor -- version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
