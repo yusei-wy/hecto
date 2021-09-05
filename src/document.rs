ee9e4f90deeeebfd138b6d3d09795c52c49595c7
yusei-wy <yusei.kasa@gmail.com>
2021-09-05 11:52:42 +0900

    change: MultiComment  を複数行で強調表示されるように
    マルチバイト文字だとおかしくなるバグが有る

--- src/document.rs
+++ src/document.rs
@@ -16,10 +16,12 @@ impl Document {
     pub fn open(filename: &str) -> Result<Self, std::io::Error> {
         let contents = fs::read_to_string(filename)?;
         let file_type = FileType::from(filename);
+        let mut start_with_comment = false;
         let mut rows = Vec::new();
         for line in contents.lines() {
             let mut row = Row::from(line);
-            row.highlight(&file_type.highlighting_options(), None, false);
+            start_with_comment =
+                row.highlight(&file_type.highlighting_options(), None, start_with_comment);
             rows.push(row);
         }
         Ok(Self {
@@ -56,9 +58,7 @@ impl Document {
         }
         #[allow(clippy::indexing_slicing)]
         let current_row = &mut self.rows[at.y];
-        let mut new_row = current_row.split(at.x);
-        current_row.highlight(&self.file_type.highlighting_options(), None, false);
-        new_row.highlight(&self.file_type.highlighting_options(), None, false);
+        let new_row = current_row.split(at.x);
         #[allow(clippy::integer_arithmetic)]
         self.rows.insert(at.y + 1, new_row);
     }
@@ -71,20 +71,16 @@ impl Document {
 
         if c == '\n' {
             self.insert_newline(at);
-            return;
-        }
-
-        if at.y == self.rows.len() {
+        } else if at.y == self.rows.len() {
             let mut row = Row::default();
             row.insert(0, c);
-            row.highlight(&self.file_type.highlighting_options(), None, false);
             self.rows.push(row);
         } else {
             #[allow(clippy::indexing_slicing)]
             let row = &mut self.rows[at.y];
             row.insert(at.x, c);
-            row.highlight(&self.file_type.highlighting_options(), None, false);
         }
+        self.highlight(None);
     }
 
     #[allow(clippy::integer_arithmetic, clippy::indexing_slicing)]
@@ -100,12 +96,11 @@ impl Document {
             let next_row = self.rows.remove(at.y + 1);
             let row = &mut self.rows[at.y];
             row.append(&next_row);
-            row.highlight(&self.file_type.highlighting_options(), None, false);
         } else {
             let row = &mut self.rows[at.y];
             row.delete(at.x);
-            row.highlight(&self.file_type.highlighting_options(), None, false);
         }
+        self.highlight(None);
     }
 
     pub fn save(&mut self) -> Result<(), Error> {
